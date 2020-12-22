ae5fa78a4d2b4fb7ee820e0c911e95d29ba0aee5
eijirou <tsujii.eijirou1689@gmail.com>
2020-12-22 23:18:01 +0900

    プロジェクト追加

---
+++ StageCreater/Assets/Plugins/UniRx/Scripts/Mine/Network.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+
+public class Network : MonoBehaviour
+{
+    bool net = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // ネットワークの状態を出力
+        switch ( Application.internetReachability ) {
+        case NetworkReachability.NotReachable:
+            Debug.LogError("ネットワークには到達不可");
+                net = true;
+            break;
+        case NetworkReachability.ReachableViaCarrierDataNetwork:
+            Debug.Log("キャリアデータネットワーク経由で到達可能");
+                net = true;
+            break;
+        case NetworkReachability.ReachableViaLocalAreaNetwork:
+            Debug.Log("Wifiまたはケーブル経由で到達可能");
+            break;
+        }
+    }
+
+    private void Start()
+    {
+        if(!net)
+        {
+            Debug.LogError("アクセスキーが違うよ^^");
+        }
+    }
+}
---
+++ StageCreater/Assets/Script/ScriptableObject/Stage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public int widthsize;
+    public int heightsize;
+    public int speed;
+    public int value;
+    public int direction;
+}
